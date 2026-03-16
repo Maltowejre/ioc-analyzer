3f4357bccd9510a646e2133460956022ea47e269
Mohammad Altowejre <matw4325@gmail.com>
2026-03-17 00:33:46 +0300

    Refactor config.py to use Streamlit

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-VT_API_KEY = os.getenv("VT_API_KEY")
-ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
-OTX_API_KEY = os.getenv("OTX_API_KEY")
+try:
+    import streamlit as st
+
+    VT_API_KEY = st.secrets.get("VT_API_KEY", os.getenv("VT_API_KEY"))
+    ABUSEIPDB_API_KEY = st.secrets.get("ABUSEIPDB_API_KEY", os.getenv("ABUSEIPDB_API_KEY"))
+    OTX_API_KEY = st.secrets.get("OTX_API_KEY", os.getenv("OTX_API_KEY"))
+
+except Exception:
+    VT_API_KEY = os.getenv("VT_API_KEY")
+    ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
+    OTX_API_KEY = os.getenv("OTX_API_KEY")
