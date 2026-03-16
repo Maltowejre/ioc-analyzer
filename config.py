7059fd028e0ad7eb72727ef17db8a5fea73d19bf
Mohammad Altowejre <matw4325@gmail.com>
2026-03-17 00:41:45 +0300

    Refactor config.py to improve API key handling with Streamlit

--- config.py
+++ config.py
@@ -1,14 +1,23 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
+VT_API_KEY = os.getenv("VT_API_KEY")
+ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
+OTX_API_KEY = os.getenv("OTX_API_KEY")
+
 try:
     import streamlit as st
 
-    VT_API_KEY = st.secrets.get("VT_API_KEY", os.getenv("VT_API_KEY"))
-    ABUSEIPDB_API_KEY = st.secrets.get("ABUSEIPDB_API_KEY", os.getenv("ABUSEIPDB_API_KEY"))
-    OTX_API_KEY = st.secrets.get("OTX_API_KEY", os.getenv("OTX_API_KEY"))
+    if "VT_API_KEY" in st.secrets:
+        VT_API_KEY = st.secrets["VT_API_KEY"]
+
+    if "ABUSEIPDB_API_KEY" in st.secrets:
+        ABUSEIPDB_API_KEY = st.secrets["ABUSEIPDB_API_KEY"]
+
+    if "OTX_API_KEY" in st.secrets:
+        OTX_API_KEY = st.secrets["OTX_API_KEY"]
 
 except Exception:
-    VT_API_KEY = os.getenv("VT_API_KEY")
-    ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
-    OTX_API_KEY = os.getenv("OTX_API_KEY")
+    pass
